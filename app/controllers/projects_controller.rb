bf7caac64c53d73e30d5f1571116e9268b879ccc
Mohammed Sadiq <sadiqmmm@gmail.com>
2016-02-13 16:07:46 +0530

    add project and bootstrape style

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,9 +1,11 @@
 class ProjectsController < ApplicationController
+	before_action :set_project, only: [:show, :edit, :update, :destroy]
+
 	def index
+		@projects = Project.all
 	end
 
-	def show
-		@project = Project.find(params[:id])
+	def show		
 	end
 
 	def new
@@ -22,7 +24,37 @@ class ProjectsController < ApplicationController
 		end  
 	end
 
+	def edit	  
+	end
+
+	def update		
+		
+		if @project.update(project_params)
+
+	  	flash[:notice] = "Project has been updated."
+	  	redirect_to @project
+		else
+		  flash.now[:alert] = "Project has not been updated."
+		  render "edit"
+		end
+	end
+
+	def destroy		
+		@project.destroy
+
+		flash[:notice] = "Project has been deleted."
+		redirect_to projects_path
+	end
+
 	private
+
+	def set_project
+		@project = Project.find(params[:id])
+	
+	 	rescue ActiveRecord::RecordNotFound
+			flash[:alert] = "The project you were looking for could not be found."
+		redirect_to projects_path
+	end
 
 	def project_params
 		params.require(:project).permit(:name, :description)
