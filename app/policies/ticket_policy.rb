c32c79e506c2e0fbffc202d7027069f352a66924
Mohammed Sadiq <sadiqmmm@gmail.com>
2016-02-17 15:13:33 +0530

    Only editors, managers and admins can create tickets

--- app/policies/ticket_policy.rb
+++ app/policies/ticket_policy.rb
@@ -8,4 +8,9 @@ class TicketPolicy < ApplicationPolicy
   def show?
 	  user.try(:admin?) || record.project.roles.exists?(user_id: user)
 	end
+
+	def create?
+	  user.try(:admin?) || record.project.has_manager?(user) ||
+	  record.project.has_editor?(user)
+	end
 end
