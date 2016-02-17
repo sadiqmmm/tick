0289851c4102a0e306209c902abd4e72d096336e
Mohammed Sadiq <sadiqmmm@gmail.com>
2016-02-17 16:17:06 +0530

    Fix Admins can edit project roles of existing users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -38,21 +38,23 @@ class Admin::UsersController < Admin::ApplicationController
 		User.transaction do
 			@user.roles.clear
 			role_data = params.fetch(:roles, [])
+			
 			role_data.each do |project_id, role_name|
 		
-			if role_name.present?
-		  	@user.roles.build(project_id: project_id, role: role_name)
-			end
-		end
+			  if role_name.present?
+		  	  @user.roles.build(project_id: project_id, role: role_name)
+			  end
+		  end
 
-		if @user.update(user_params)
-		  flash[:notice] = "User has been updated."
-		  redirect_to admin_users_path
-		else
-		  flash.now[:alert] = "User has not been updated."
-		  render "edit"
-		  raise ActiveRecord::Rollback
-		end
+			if @user.update(user_params)
+			  flash[:notice] = "User has been updated."
+			  redirect_to admin_users_path
+			else
+			  flash.now[:alert] = "User has not been updated."
+			  render "edit"
+			  raise ActiveRecord::Rollback
+			end
+		end	
 	end
 	
 	def archive
