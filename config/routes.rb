37e6221ad85579ef916c0747e149ec451813d155
Mohammed Sadiq <sadiqmmm@gmail.com>
2016-02-17 16:56:54 +0530

    Uploaded attachments are now served through the AttachmentsController for extra security

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,8 @@ Rails.application.routes.draw do
 
   devise_for :users
 
+  resources :attachments, only: [:show]
+
   resources :projects, only: [:index, :show, :edit, :update] do 
     resources :tickets
   end
