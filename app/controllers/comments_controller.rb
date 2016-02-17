37d340bf987667e90c7fed768f7ef2da78343642
Mohammed Sadiq <sadiqmmm@gmail.com>
2016-02-17 18:51:21 +0530

    Protect state_id from users who do not have permission to change it

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,7 +7,14 @@ class CommentsController < ApplicationController
 	end
 	
 	def create
-		@comment = @ticket.comments.build(comment_params)
+
+		whitelisted_params = comment_params
+		
+		unless policy(@ticket).change_state?
+		  whitelisted_params.delete(:state_id)
+		end
+		@comment = @ticket.comments.build(whitelisted_params)
+
 		@comment.author = current_user
 		authorize @comment, :create?
 		
